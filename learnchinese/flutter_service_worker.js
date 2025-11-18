4b280d1c5e61964542b134f4f8a7d1b3da2ad0eb
zhangzhijiang <zhangzhijiang@users.noreply.github.com>
2025-11-18 00:09:24 -0800

    added learnchinese

--- learnchinese/flutter_service_worker.js
+++ learnchinese/flutter_service_worker.js
@@ -88,7 +88,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4e1816c97019b29a4b209d06fa3b3f42
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ef3bf6e8feaab51c7245e2fb39c9d7a2",
+"flutter_bootstrap.js": "0d84b1b2a229f85e9ff3869773a8912e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
