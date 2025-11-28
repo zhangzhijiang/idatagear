ea4ce3c559d0fa69b3752158aa0c93c8d0047101
zhangzhijiang <zhangzhijiang@users.noreply.github.com>
2025-11-27 21:24:06 -0800

    rebuild testlearnchinese

--- testlearnchinese/flutter_service_worker.js
+++ testlearnchinese/flutter_service_worker.js
@@ -91,7 +91,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8a4af37b50602b68e5b4e3542d22c929
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "167556f6b0b07733aa980dcc0d0b2f06",
+"flutter_bootstrap.js": "79666efb83674632cd360dbed1dde175",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
