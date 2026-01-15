0a0c4eacaa0873374d284c5ce125f365f1bcd298
zhangzhijiang <zhangzhijiang@users.noreply.github.com>
2026-01-15 11:23:20 -0800

    added more

--- gcd/flutter_bootstrap.js
+++ gcd/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"cb467e31a54207dc987aca5bb0941c4d2e3fd9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1885915542"
+    serviceWorkerVersion: "4054182090"
   }
 });
