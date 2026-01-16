8997ca9216974665fb8beaafb918d5739de4a776
zhangzhijiang <zhangzhijiang@users.noreply.github.com>
2026-01-15 22:30:43 -0800

    updated LearnCantonese new web version

--- learncantonese/flutter_bootstrap.js
+++ learncantonese/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"cb467e31a54207dc987aca5bb0941c4d2e3fd9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "176466759"
+    serviceWorkerVersion: "4209843733"
   }
 });
