32d20a15e694b392bdbc72e1876ef864979339a7
zhangzhijiang <zhangzhijiang@users.noreply.github.com>
2026-06-19 23:28:19 -0700

    fixed the issue

--- hiking/flutter_bootstrap.js
+++ hiking/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "850854872" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3166830039" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
