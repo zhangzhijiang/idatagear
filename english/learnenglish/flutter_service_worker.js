345bd03f75d212823d99242ddeb28dbfa0ee0627
zhangzhijiang <zhangzhijiang@gmail.com>
2026-08-02 18:07:39 -0700

    restructure english, chinese and cantonese sites

---
+++ english/learnenglish/flutter_service_worker.js
@@ -0,0 +1,291 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"assets/AssetManifest.bin": "849b2b13aec42464320211fb371b2cd2",
+"assets/AssetManifest.bin.json": "f9c3541440a4f2269b9270459a8a8c97",
+"assets/assets/hsk/articles/a1_articles.json": "09bfac0ee1e57b06ad8ae4283e4964b0",
+"assets/assets/hsk/articles/a2_articles.json": "6ae5ee444d63cb1f20e03fdee56d966c",
+"assets/assets/hsk/articles/b1_articles.json": "c2174e09fff8d1183b9b1471a5878cf9",
+"assets/assets/hsk/articles/b2_articles.json": "e8bbdec7276d95b082dfe07fdfee6afb",
+"assets/assets/hsk/articles/c1_articles.json": "ef5c5fc2b529757368870f5399d45cee",
+"assets/assets/hsk/articles/c2_articles.json": "afd595669227f7ef0124977464afffa6",
+"assets/assets/hsk/questions/a1_questions.json": "7d863730da7103bd8202b00567fd77ab",
+"assets/assets/hsk/questions/a2_questions.json": "f5b92a776e42ae9f18dbcfee7aa52291",
+"assets/assets/hsk/questions/b1_questions.json": "3b06b695871416cad7f72e9777bd60e6",
+"assets/assets/hsk/questions/b2_questions.json": "3a1b24c736b6756331aa4c372c55cbd3",
+"assets/assets/hsk/questions/c1_questions.json": "ac1cf0b555be17707caa958f2e9d0ca5",
+"assets/assets/hsk/questions/c2_questions.json": "29a2f85dfa8de665e5c9db7f95fcb98d",
+"assets/assets/hsk/sentences/a1_sentences.json": "0244dd54372b22630694e917fd1db9ae",
+"assets/assets/hsk/sentences/a2_sentences.json": "927d37006956cabb55ca38953457656e",
+"assets/assets/hsk/sentences/b1_sentences.json": "c180493ff893101febbf728db68fea6c",
+"assets/assets/hsk/sentences/b2_sentences.json": "3515e579774797c0ff52741c17e5debb",
+"assets/assets/hsk/sentences/c1_sentences.json": "1fe4928d83a30bcbe3ba4ee2be2ab1a2",
+"assets/assets/hsk/sentences/c2_sentences.json": "b5af7ac9d675456a99abd248ca0c73ae",
+"assets/assets/hsk/tests/a1_tests.json": "bdba9496115ed0ca2adc4ead2785aa5f",
+"assets/assets/hsk/tests/a2_tests.json": "b2820dc5c1a390adbd3527cf48125991",
+"assets/assets/hsk/tests/b1_tests.json": "ff7cc20a08f9a2a3126a1d338d951177",
+"assets/assets/hsk/tests/b2_tests.json": "ad55441eb2c802e8d10f9461279bbc35",
+"assets/assets/hsk/tests/c1_tests.json": "e6467b209259426ba7519c0cd078183a",
+"assets/assets/hsk/tests/c2_tests.json": "8b712c08dbce465dc6a10bcafcb894df",
+"assets/assets/hsk/user_favorites.json": "35d0e0b801444bb1f4c2115720f8e4f6",
+"assets/assets/hsk/words/a1_words.json": "ea1b066031fa8eea47e56f3ddb259695",
+"assets/assets/hsk/words/a2_words.json": "f6d445ab4275d1ad5d4196895a2b1093",
+"assets/assets/hsk/words/b1_words.json": "c3e6b1946acb6d57a8e64d1475d87f70",
+"assets/assets/hsk/words/b2_words.json": "b56790f7f4619a64d1d4fba64051f507",
+"assets/assets/hsk/words/c1_words.json": "ca72d8c61f715c7c6178de81f37814b7",
+"assets/assets/hsk/words/c2_words.json": "d757b8ccb869348670d7b5a4dc473d74",
+"assets/assets/images/google_play_store_logo.png": "48f12f94c175de93a88dda60ae468fef",
+"assets/assets/images/great_cantonese_dict_logo.png": "88abe8909c020cfdfa45db2268312b2d",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_21.png": "4db96939be7e13bcda61d2e3fe8a9b55",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_22.png": "dc324696be949aaf235ff88ac7dca29b",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_23.png": "c1e7ad32ea8b65fdcdcde4f3eef08431",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_24.png": "f318b2fffdb466cacef28718e6da9b6d",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_25.png": "ff172c48805da73fcb3c0b87d58043b8",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_26.png": "cf22ae8c1a71d407b904b2d9449163b9",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_27.png": "4370105381bba9bc141f7cebb48b9c82",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_28.png": "aaa786197a5e4e1581d8a0beebf073c1",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_29.png": "224dc8afb1ac79be37fe9f417b6ec266",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_30.png": "01ddbc18bda29c0b5cf2721b84da7ad4",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_32.png": "4370105381bba9bc141f7cebb48b9c82",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_43.png": "f4d9730ebd6e74b4d3af1935171c2bc6",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_50.png": "b1fa8b4729429470b648c62ff5182dac",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_53.png": "c63b4c7274cb8f87161f3b3d4f0f488e",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_54.png": "39ed08915a9c3ed42bd592b95217076d",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_58.png": "fc5dd2f4db87d4d54c2e0be624503db0",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_59.png": "667e49be2491f2306dbbfc87a0443496",
+"assets/assets/images/hsk/questions/hsk_HSK1_question_60.png": "2c46290d004177d2a4e6e296d687ed35",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_101.png": "ef32b5eee51c60b99ef422e86227c006",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_104_A.png": "a215d164649b192f61c2481b37433108",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_104_B.png": "acda80d2400d372b16486ebdd4e9c0a8",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_104_C.png": "51c92ff99a4af2c8f96345dff3dfc696",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_104_D.png": "22c4f2d255fa70c475ddc57e7adc0c62",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_106.png": "9007dd9e86fd8a0601d74b0d403eebca",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_109_A.png": "25918758acae61963c6328d7562d98ea",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_109_B.png": "dd12f515c4580a74e960ca1f71c90cfe",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_109_C.png": "c66759ca6aea284356e6b3318dcabac0",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_109_D.png": "ea58bf3fcbbf5737ce4d6a7cec30f735",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_112.png": "18c10e5f2a9a3bed523e5c6a8fbf7718",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_115.png": "d1c2ebfab5ebd2461e3b82570bd7bef3",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_118.png": "488179599fc115f40e3f58ef95ab217e",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_121_A.png": "84481d7a78b2763b08f3db7dca45ee0f",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_121_B.png": "59522a4c9ace145136a0b313ec52f3dc",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_121_C.png": "7a23028b4d021bf2df7f426b6038b765",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_121_D.png": "25e0a8df58888f3574ec68b83275289f",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_125.png": "6da0065fe2f838a0d23183deb8fa3ab8",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_127_A.png": "22c4f2d255fa70c475ddc57e7adc0c62",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_127_B.png": "51c92ff99a4af2c8f96345dff3dfc696",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_127_C.png": "f1120f189b54ccb500b24519912022be",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_127_D.png": "b86654c3bc9efc439cd838e2a51e173f",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_130.png": "25918758acae61963c6328d7562d98ea",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_133.png": "ee5854335128f7067093851dcb4b7430",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_136.png": "d5fce48241a54ee4e1350d735461df23",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_136_A.png": "bf6a0b29e915be1ff8117a30a1e7d7ea",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_136_B.png": "bf6a0b29e915be1ff8117a30a1e7d7ea",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_136_C.png": "bf6a0b29e915be1ff8117a30a1e7d7ea",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_136_D.png": "bf6a0b29e915be1ff8117a30a1e7d7ea",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_140.png": "dabd40cb87dfd3314f76bc4f2d782f36",
+"assets/assets/images/hsk/questions/hsk_HSK2_question_143.png": "ca16789a7e8e4f429ab2303283d79427",
+"assets/assets/images/learn_cantonese_logo.png": "845ac5b92c9d7840f3ee9ef39fcd7401",
+"assets/assets/images/learn_chinese_logo.png": "15307073e111b65d0ee4d4f6881df446",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/fonts/MaterialIcons-Regular.otf": "fbf1a8398e09c11422fa3dc351444564",
+"assets/NOTICES": "c0038606bd96ac42aaeafdb5d53c01a6",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
+"canvaskit/canvaskit.js": "8331fe38e66b3a898c4f37648aaf7ee2",
+"canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
+"canvaskit/canvaskit.wasm": "9b6a7830bf26959b200594729d73538e",
+"canvaskit/chromium/canvaskit.js": "a80c765aaa8af8645c9fb1aae53f9abf",
+"canvaskit/chromium/canvaskit.js.symbols": "e2d09f0e434bc118bf67dae526737d07",
+"canvaskit/chromium/canvaskit.wasm": "a726e3f75a84fcdf495a15817c63a35d",
+"canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
+"canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
+"canvaskit/skwasm.wasm": "7e5f3afdd3b0747a1fd4517cea239898",
+"canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
+"canvaskit/skwasm_heavy.js.symbols": "0755b4fb399918388d71b59ad390b055",
+"canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
+"flutter_bootstrap.js": "d6a12c410eb2224a8172d44f0571dc1c",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "8160dcf271c4b6c1be2623f18da0cb98",
+"/": "8160dcf271c4b6c1be2623f18da0cb98",
+"main.dart.js": "5c5c338f844a386634d695fadc97e3df",
+"manifest.json": "c871d9ee0bf2aff00a0c0d36065c4fcf",
+"sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118",
+"version.json": "ac760e817b76ecb6457482c7b8c5b596"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
